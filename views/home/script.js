500930631cebf6b858ab17b4ba38838fa536a417
Pedro Henrique Arruda <pedrohenriquearruda158@gmail.com>
2024-10-05 22:15:36 -0300

    Alterações na página inicial
    Ajuste no nome das classes; adição de seções.

--- views/home/script.js
+++ views/home/script.js
@@ -1,9 +1,4 @@
 window.addEventListener("scroll", function(){
-    let header = this.document.querySelector('#header')
-    header.classList.toggle('rolagem', window.scrollY > 200)
-})
-
-window.addEventListener("scroll", function(){
-    let logo = this.document.querySelector('#logo')
-    logo.classList.toggle('rolagem', window.scrollY > 0)
+    let navbar = this.document.querySelector('#navbar')
+    navbar.classList.toggle('rolagem', window.scrollY > 200)
 })
