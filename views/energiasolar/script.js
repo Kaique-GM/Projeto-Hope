517d1d1f548db0f10b09221ff3ab27f808528ae4
Pedro Henrique Arruda <pedrohenriquearruda158@gmail.com>
2024-10-23 13:33:27 -0300

    Ultimos ajustes pré-AC2
    Alterações no footer; alterações na página de energia solar e página principal.

--- views/energiasolar/script.js
+++ views/energiasolar/script.js
@@ -1,4 +1,14 @@
 window.addEventListener("scroll", function(){
     let navbar = this.document.querySelector('#navbar')
-    navbar.classList.toggle('rolagem', window.scrollY > 200)
+    navbar.classList.toggle('rolagem', window.scrollY > 100)
+})
+
+
+const accordions = document.querySelectorAll('.esolar-duvidas__accordion');
+
+accordions.forEach(accordion => {
+    accordion.addEventListener('click', () => {
+        const body = accordion.querySelector('.esolar-duvidas__accordion-body');
+        body.classList.toggle('active');
+    })
 })
