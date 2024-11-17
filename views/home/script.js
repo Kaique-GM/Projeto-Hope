d04162eb942f061f16a70491f961e19518da17c2
Pedro Henrique Arruda <pedrohenriquearruda158@gmail.com>
2024-11-17 14:57:08 -0300

    Alterações Página Inicial, Energia Elétrica e Hidrelétrica

--- views/home/script.js
+++ views/home/script.js
@@ -8,12 +8,13 @@ window.addEventListener("scroll", function () {
 const energiasRenovaveisItem = document.querySelector('.dropdown'); // Item do menu dropdown
 const navbar = document.getElementById('navbar'); // Navbar onde será adicionada a classe
 
+
 // Função para adicionar a classe .rolagem ao passar o mouse
 energiasRenovaveisItem.addEventListener('mouseenter', () => {
     navbar.classList.add('rolagem'); // Adiciona a classe .rolagem
 });
 
-// Função para remover a classe .rolagem quando o mouse sai
+
 energiasRenovaveisItem.addEventListener('mouseleave', () => {
-    navbar.classList.remove('rolagem'); // Remove a classe .rolagem
+        navbar.classList.remove('rolagem'); // Remove a classe .rolagem
 });
